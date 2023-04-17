f94a5a17a62a02927d4b0a8d67346ece959f6249
yogifang <yogifang@gmail.com>
2023-04-17 10:16:54 +0800

    Ver1.5

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1952,7 +1952,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "fmMain";
-            this.Text = "金戰神EM02-E半測系統V1.3";
+            this.Text = "金戰神EM02-E半測系統V1.5";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FmMain_FormClosed);
             this.Load += new System.EventHandler(this.FmMain_Load);
             this.groupBox1.ResumeLayout(false);
